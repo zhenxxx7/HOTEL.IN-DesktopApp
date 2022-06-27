c8715914905cbcdf8d4d989e85940623174a54b2
zhenxxx7 <jordy.zeventh@gmail.com>
2022-06-28 00:03:20 +0700

    Update Query Crystal Report

--- visproProject/Main.Designer.cs
+++ visproProject/Main.Designer.cs
@@ -267,12 +267,12 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Poppins ExtraBold", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Poppins ExtraBold", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(-2, 22);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(160, 60);
+            this.label4.Size = new System.Drawing.Size(180, 60);
             this.label4.TabIndex = 115;
-            this.label4.Text = "Hotel.in";
+            this.label4.Text = "HOTEL.IN";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Main
