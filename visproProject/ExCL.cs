db399912d4d0705e348871c480bb764aba042159
zhenxxx7 <jordy.zeventh@gmail.com>
2022-06-30 02:56:52 +0700

    update Exit/Close Dialog

--- visproProject/ExCL.cs
+++ visproProject/ExCL.cs
@@ -24,9 +24,24 @@ namespace visproProject
 
         private void CloseBtn_Click(object sender, EventArgs e)
         {
+            this.Close();
+
+            Main obj = (Main)Application.OpenForms["Main"];
+            AdminForm obj2 = (AdminForm)Application.OpenForms["AdminForm"];
+
+            if (obj2 == null)
+            {
+
+                obj.Close();
+            }
+            else
+            {
+
+                obj2.Close();
+            }
+
             Login login = new Login();
             login.Show();
-            this.Hide();
         }
     }
 }
